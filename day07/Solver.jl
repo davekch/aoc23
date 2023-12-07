5c7bab4e7b950fbe753e5818daae96ced8e03d60
davekch <dave-koch@web.de>
2023-12-07 08:16:56 +0100

    reduce code duplication

--- day07/Solver.jl
+++ day07/Solver.jl
@@ -26,9 +26,9 @@ export parse_input
     Five
 end
 
-function get_handtype(hand)
-    uniqueness = unique(hand) |> length
-    card_counts = counter(hand) |> values
+
+"""get the handtype based on the uniqueness and counts of the cards"""
+function get_handtype_from_properties(uniqueness, card_counts)
     if uniqueness == 1
         return Five
     elseif uniqueness == 2
@@ -50,32 +50,40 @@ function get_handtype(hand)
     end
 end
 
+function get_handtype(hand)
+    uniqueness = unique(hand) |> length
+    card_counts = counter(hand) |> values
+    get_handtype_from_properties(uniqueness, card_counts)
+end
+
 const cardrank = Dict([c=>v for (v,c) in enumerate("23456789TJQKA")])
 
-function isless_hand(hand1, hand2)
-    # defines total ordering for hands
-    type1, type2 = get_handtype.([hand1, hand2])
-    if type1 < type2
-        return true
-    elseif type1 == type2
-        for (c1,c2) in zip(hand1, hand2)
-            if c1 == c2
-                continue
-            elseif cardrank[c1] < cardrank[c2]
-                return true
-            else
-                return false
+"""return a isless-function based on the hand calculation and ranks"""
+function isless_generator(calc_hand, ranks)
+    function isless_hand(hand1, hand2)
+        # defines total ordering for hands
+        type1, type2 = calc_hand.([hand1, hand2])
+        if type1 < type2
+            return true
+        elseif type1 == type2
+            for (c1,c2) in zip(hand1, hand2)
+                if c1 == c2
+                    continue
+                elseif ranks[c1] < ranks[c2]
+                    return true
+                else
+                    return false
+                end
             end
+        else
+            return false
         end
-    else
-        return false
     end
 end
-export isless_hand
 
 
 function solve1(parsed)
-    sorted_by_rank = sort(parsed, by=x->x[1], lt=isless_hand)
+    sorted_by_rank = sort(parsed, by=x->x[1], lt=isless_generator(get_handtype, cardrank))
     result = 0
     for (rank, (hand, bid)) in enumerate(sorted_by_rank)
         result += rank * bid
@@ -102,51 +110,13 @@ function get_handtype_j(hand)
         most, count = sort(card_counts|>collect, by=p->p[2])[end]
         card_counts[most] = count + jokers
     end
-    if uniqueness == 1
-        return Five
-    elseif uniqueness == 2
-        if any([c == 4 for c in card_counts |> values])
-            return Four
-        else
-            return Fullhouse
-        end
-    elseif uniqueness == 3
-        if any([c == 3 for c in card_counts |> values])
-            return Three
-        else
-            return Twopair
-        end
-    elseif uniqueness == 4
-        return Onepair
-    else
-        return Highcard
-    end
+    get_handtype_from_properties(uniqueness, card_counts|>values)
 end
 
 const cardrank_j = Dict([c=>v for (v,c) in enumerate("J23456789TQKA")])
 
-function isless_hand_j(hand1, hand2)
-    # defines total ordering for hands
-    type1, type2 = get_handtype_j.([hand1, hand2])
-    if type1 < type2
-        return true
-    elseif type1 == type2
-        for (c1,c2) in zip(hand1, hand2)
-            if c1 == c2
-                continue
-            elseif cardrank_j[c1] < cardrank_j[c2]
-                return true
-            else
-                return false
-            end
-        end
-    else
-        return false
-    end
-end
-
 function solve2(parsed)
-    sorted_by_rank = sort(parsed, by=x->x[1], lt=isless_hand_j)
+    sorted_by_rank = sort(parsed, by=x->x[1], lt=isless_generator(get_handtype_j, cardrank_j))
     result = 0
     for (rank, (hand, bid)) in enumerate(sorted_by_rank)
         result += rank * bid
