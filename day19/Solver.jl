351c869bbcea2c7d6687b7989b8c0e717649f19c
davekch <dave-koch@web.de>
2023-12-19 22:42:19 +0100

    day 19 part 2 ugly af

--- day19/Solver.jl
+++ day19/Solver.jl
@@ -2,6 +2,8 @@ module Solver
 using Test
 using AoC
 using AoC.Utils
+using DataStructures
+import Combinatorics: combinations
 
 
 struct Part
@@ -96,7 +98,223 @@ end
 export solve1
 
 
+const Range = NamedTuple{(:minx,:maxx,:minm,:maxm,:mina,:maxa,:mins,:maxs), Tuple{Int,Int,Int,Int,Int,Int,Int,Int}}
+
+
+# i apologize for this "function"
+function rules_to_ranges(rules)
+    # turn rules into a list of ranges of x,m,a,s to be accepted
+    ranges = Range[]
+    # queue ranges, the name of the rule and index of the rule to check
+    to_check = Queue{Tuple{Range, String, Int}}()
+    start = Range((1,4000,1,4000,1,4000,1,4000))
+    enqueue!(to_check, (start, "in", 1))
+    while !isempty(to_check)
+        range, rulekey, index = dequeue!(to_check)
+        if rulekey == "A"
+            push!(ranges, range)
+            continue
+        elseif rulekey == "R"
+            continue
+        end
+
+        condition = rules[rulekey][index]
+        @debug "checking $condition ($rulekey[$index]) on $range"
+        if condition == "A"
+            @debug "  it's a match!"
+            push!(ranges, range)
+        elseif condition == "R"
+            @debug "  reject"
+            continue
+        elseif typeof(condition) <: AbstractString
+            @debug "  pass on to $condition"
+            enqueue!(to_check, (range, condition, 1))
+        else
+            minx,maxx,minm,maxm,mina,maxa,mins,maxs = range
+            # pass the range that satisfies the condition on to the rule where it is 
+            # sent to, and the range that doesnt to the next index of this rule
+            # i dont know a less verbose way to do this; monstrosity ahead
+            if condition.attr == :x
+                if condition.comp == (<)
+                    if condition.value <= minx
+                        # none of the x-values make it; move the entire range to the next rule
+                        enqueue!(to_check, (range, rulekey, index+1))
+                        @debug "  range outside condition, queue $(last(to_check))"
+                    elseif minx < condition.value <= maxx
+                        @debug "  range partially in condition, queue"
+                        # some x-values make it, others don't
+                        range1 = Range((minx, condition.value-1, minm,maxm,mina,maxa,mins,maxs))
+                        # this range fulfils the condition, move it to the next rule
+                        enqueue!(to_check, (range1, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((condition.value, maxx, minm,maxm,mina,maxa,mins,maxs))
+                        # this range doesn't fulfil the condition, send it one index further
+                        enqueue!(to_check, (range2, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                    else
+                        # the entire range fulfils the condition
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                else  # condition.comp = >
+                    if condition.value >= maxx
+                        # none of the x-values make it; move the entire range to the next rule
+                        enqueue!(range, rulekey, index+1)
+                        @debug "  range outside condition, queue $(last(to_check))"
+                    elseif minx <= condition.value < maxx
+                        @debug "  range partially in condition, queue"
+                        # some x-values make it, others don't
+                        range1 = Range((minx, condition.value, minm,maxm,mina,maxa,mins,maxs))
+                        # this range doesnt fulfil the condition, move it to the next index
+                        enqueue!(to_check, (range1, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((condition.value+1, maxx, minm,maxm,mina,maxa,mins,maxs))
+                        # this range fulfils the condition, send it to the next rule
+                        enqueue!(to_check, (range2, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                    else
+                        # the entire range fulfils the condition
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                end
+            elseif condition.attr == :m
+                if condition.comp == (<)
+                    if condition.value <= minm
+                        enqueue!(to_check, (range, rulekey, index+1))
+                        @debug "  range outside condition, queue $(last(to_check))"
+                    elseif minm < condition.value <= maxm
+                        @debug "  range partially in condition, queue"
+                        range1 = Range((minx,maxx,minm,condition.value-1,mina,maxa,mins,maxs))
+                        enqueue!(to_check, (range1, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((minx,maxx,condition.value,maxm,mina,maxa,mins,maxs))
+                        enqueue!(to_check, (range2, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                    else
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                else  # condition.comp = >
+                    if condition.value >= maxm
+                        enqueue!(range, rulekey, index+1)
+                        @debug "  range outside condition, queue $(last(to_check))"
+                    elseif minm <= condition.value < maxm
+                        @debug "  range partially in condition, queue"
+                        range1 = Range((minx,maxx,minm,condition.value,mina,maxa,mins,maxs))
+                        enqueue!(to_check, (range1, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((minx,maxx,condition.value+1,maxm,mina,maxa,mins,maxs))
+                        enqueue!(to_check, (range2, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                    else
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                end
+            elseif condition.attr == :a
+                if condition.comp == (<)
+                    if condition.value <= mina
+                        enqueue!(range, rulekey, index+1)
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    elseif mina < condition.value <= maxa
+                        @debug "  range partially in condition, queue"
+                        range1 = Range((minx,maxx,minm,maxm,mina,condition.value-1,mins,maxs))
+                        enqueue!(to_check, (range1, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((minx,maxx,minm,maxm,condition.value,maxa,mins,maxs))
+                        enqueue!(to_check, (range2, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                    else
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                else  # condition.comp = >
+                    if condition.value >= maxa
+                        enqueue!(range, rulekey, index+1)
+                        @debug "  range outside condition, queue $(last(to_check))"
+                    elseif mina <= condition.value < maxa
+                        @debug "  range partially in condition, queue"
+                        range1 = Range((minx,maxx,minm,maxm,mina,condition.value,mins,maxs))
+                        enqueue!(to_check, (range1, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((minx,maxx,minm,maxm,condition.value+1,maxa,mins,maxs))
+                        enqueue!(to_check, (range2, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                    else
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                end
+            elseif condition.attr == :s
+                if condition.comp == (<)
+                    if condition.value <= mins
+                        enqueue!(range, rulekey, index+1)
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    elseif mins < condition.value <= maxs
+                        @debug "  range partially in condition, queue"
+                        range1 = Range((minx,maxx,minm,maxm,mina,maxa,mins,condition.value-1))
+                        enqueue!(to_check, (range1, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((minx,maxx,minm,maxm,mina,maxa,condition.value,maxs))
+                        enqueue!(to_check, (range2, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                    else
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                else  # condition.comp = >
+                    if condition.value >= maxs
+                        enqueue!(range, rulekey, index+1)
+                        @debug "  range outside condition, queue $(last(to_check))"
+                    elseif mins <= condition.value < maxs
+                        @debug "  range partially in condition, queue"
+                        range1 = Range((minx,maxx,minm,maxm,mina,maxa,mins,condition.value))
+                        enqueue!(to_check, (range1, rulekey, index+1))
+                        @debug "    $(last(to_check))"
+                        range2 = Range((minx,maxx,minm,maxm,mina,maxa,condition.value+1,maxs))
+                        enqueue!(to_check, (range2, condition.send, 1))
+                        @debug "    $(last(to_check))"
+                    else
+                        enqueue!(to_check, (range, condition.send, 1))
+                        @debug "  range inside condition, queue $(last(to_check))"
+                    end
+                end
+            end
+        end
+    end
+    ranges
+end
+
+
+function intersections_area(ranges)
+    A = 0
+    # calculate pairwise intersection areas and add them up
+    for (r1,r2) in combinations(ranges, 2)
+        A += (
+            length(intersect(r1.minx:r1.maxx, r2.minx:r2.maxx))
+            * length(intersect(r1.minm:r1.maxm, r2.minm:r2.maxm))
+            * length(intersect(r1.mina:r1.maxa, r2.mina:r2.maxa))
+            * length(intersect(r1.mins:r1.maxs, r2.mins:r2.maxs))
+        )
+    end
+    A
+end
+
+
 function solve2(parsed)
+    rules, _ = parsed
+    ranges = rules_to_ranges(rules)
+    result = 1
+    for r in ranges
+        a = (r.maxx - r.minx + 1) * (r.maxm - r.minm + 1) * (r.maxa - r.mina + 1) * (r.maxs - r.mins + 1)
+        result += a
+        # println(r)
+        # println(a)
+        # println(result)
+    end
+    # dont ask about the -1 😭
+    result - intersections_area(ranges) - 1
 end
 export solve2
 
