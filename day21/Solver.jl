03f1473eb6de4d887e0dd6f1b285738cd5fb69dc
davekch <dave-koch@web.de>
2023-12-21 23:40:05 +0100

    day 21 part 2 failed attempt

--- day21/Solver.jl
+++ day21/Solver.jl
@@ -7,23 +7,25 @@ using DataStructures
 
 
 function parse_input(raw_data)
-    rocks = Set{Point2D}()
+    grid = Dict{Point2D, Char}()
     start = Point2D(1, 1)  # initial value
     for (y, line) in raw_data |> strip |> lines |> enumerate
         for (x, c) in line |> enumerate
-            if c == '#'
-                push!(rocks, Point2D(x, y))
+            if c in "#."
+                grid[Point2D(x, y)] = c
             elseif c == 'S'
+                grid[Point2D(x, y)] = '.'
                 start = Point2D(x, y)
             end
         end
     end
-    start, rocks
+    start, grid
 end
 export parse_input
 
 
-function solve(nsteps, (start, rocks))
+function solve(nsteps, (start, grid))
+    rocks = keys(filter(p->p[2]=='#', grid))
     reached = 0
     current = start
     q = Set{Point2D}()
@@ -39,9 +41,29 @@ function solve(nsteps, (start, rocks))
             end
         end
         reached = length(newq)
+        # debug stuff for part 2 --------------------
+        println("after $i steps: $reached")
+        printable = Dict([
+            p => (p.x ∈ 1:11 && p.y ∈ 1:11) ? "\033[48;5;57m$c\033[0m" : c
+            for (p, c) in grid ∪ Dict(newq.=>'O')
+        ])
+        println(grid_to_string(printable))
+        # println("reached in original grid: $(length(filter(p -> p.x ∈ 1:11 && p.y ∈ 1:11, newq)))")
+        sleep(0.1)
+        # -------------------------------------------
         q = newq
     end
     reached
+end
+
+
+# for debugging & printing
+function repeat_grid(grid; n=2)
+    _,maxx,_,maxy = corners(keys(grid))
+    Dict(union([
+        Dict([p + Point2D(i * maxx, j * maxy) => c for (p, c) in grid])
+        for i = -n:n, j = -n:n
+    ]...))
 end
 
 
@@ -51,7 +73,39 @@ end
 export solve1
 
 
+function solve_infinite(nsteps, (start, grid))
+    minx,maxx,miny,maxy = corners(keys(grid))
+    reached = 0
+    current = start
+    q = DefaultDict{Point2D, Int}(0)
+    q[current] = 1
+    for i = 1:nsteps
+        newq = DefaultDict{Point2D, Int}(0)
+        while !isempty(q)
+            current, count = pop!(q)
+            for n ∈ neighbours4(current)
+                if grid[Point2D(mod(n.x, minx:maxx), mod(n.y, miny:maxy))] != '#' && n ∉ keys(newq)
+                    newq[n] += count
+                end
+            end
+        end
+        # wrap outside points back to inside of the grid
+        wrapped = DefaultDict{Point2D, Int}(0)
+        for (p,c) ∈ newq
+            wrapped[Point2D(mod(p.x, minx:maxx), mod(p.y, miny:maxy))] += c
+        end
+        reached = sum(values(wrapped))
+        println("after $i steps: $reached")
+        println(grid_to_string(Dict(grid ∪ wrapped)))
+        q = wrapped
+    end
+    reached
+end
+export solve_infinite
+
+
 function solve2(parsed)
+    solve_infinite(26501365, parsed)
 end
 export solve2
 
@@ -59,7 +113,8 @@ export solve2
 solution = Solution(parse_input, solve1, solve2)
 testsolution = Solution(parse_input, p -> solve(6, p), solve2)
 
-testinput = """...........
+testinput = """
+...........
 .....###.#.
 .###.##..#.
 ..#.#...#..
